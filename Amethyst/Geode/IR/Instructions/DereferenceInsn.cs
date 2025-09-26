1cfb515c8efe98dbbe9505aed98c397ee720631b
kinderhead <67707412+kinderhead@users.noreply.github.com>
2025-09-26 15:37:57 -0600

    What happened to this file?

---
+++ Amethyst/Geode/IR/Instructions/DereferenceInsn.cs
@@ -0,0 +1,23 @@
+using Amethyst.Geode.Types;
+using Amethyst.Geode.Values;
+using Datapack.Net.Data;
+
+namespace Amethyst.Geode.IR.Instructions
+{
+    public class DereferenceInsn(ValueRef ptr) : Instruction([ptr])
+    {
+        public override string Name => "deref";
+        public override NBTType?[] ArgTypes => [null];
+
+        public override TypeSpecifier ReturnType => ((ReferenceTypeSpecifier)ptr.Type).Inner;
+
+        public override void Render(RenderContext ctx)
+        {
+            var ret = ReturnValue.Expect();
+            if (ret is ScoreValue score) ctx.Call("amethyst:core/ref/set-score", new LiteralValue($"{score.Target.Get()} {score.Score.Name}"), Arg<ValueRef>(0).Expect());
+            else ctx.Call("amethyst:core/ref/set-ref", ret, Arg<ValueRef>(0).Expect());
+        }
+
+        protected override Value? ComputeReturnValue(FunctionContext ctx) => null;
+    }
+}
