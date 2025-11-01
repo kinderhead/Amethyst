286add7443b70e67137883c7723ef66ec7c57aab
kinderhead <67707412+kinderhead@users.noreply.github.com>
2025-11-01 13:31:28 -0600

    Made homepage more mobile friendly

--- docs/src/components/Home/start.tsx
+++ docs/src/components/Home/start.tsx
@@ -1,12 +1,12 @@
+import Link from '@docusaurus/Link';
 import Heading from '@theme/Heading';
 import styles from './home.module.css';
-import Link from '@docusaurus/Link';
 
 export default function Start() {
     return (
         <div style={{ paddingTop: "3rem", paddingBottom: "3rem", display: "flex", alignItems: "center", flexDirection: "column", width: "100%" }}>
             <Heading className={styles.heading} as="h1">Get started</Heading>
-            <div style={{ display: "flex", gap: "0.5rem" }}>
+            <div className={styles.buttonGroup}>
                 <Link className="button button--success button--lg" to="/docs/tutorial/installing/">Tutorial</Link>
                 <Link className="button button--primary button--lg" to="/docs/category/language-features/">Handbook</Link>
             </div>
