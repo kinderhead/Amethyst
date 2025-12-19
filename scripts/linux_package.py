6d0ce19555a7f2050650cc6d6dbcc41737050eb0
kinderhead <67707412+kinderhead@users.noreply.github.com>
2025-12-19 12:28:18 -0500

    Change file permissions

--- scripts/linux_package.py
+++ scripts/linux_package.py
@@ -16,5 +16,7 @@ data = ET.parse("Amethyst/Amethyst.csproj").getroot()
 version = str(data.find("PropertyGroup").find("Version").text)  # type: ignore
 arch = platform.uname().machine
 
+call("chmod 775 Amethyst/dist/amethyst")
+
 for (pkg, ext, icu) in [("deb", ".deb", "libicu-dev"), ("pacman", ".pkg.tar.zst", "icu")]:
     call(f'fpm -s dir -t {pkg} --name amethyst --license mit --version {version.replace("-", "_") if pkg == "pacman" else version} --description "A programming language for Minecraft data packs" --url "https://www.amethyst.dev" --maintainer "kinderhead" -a {arch} -d {icu} -p amethyst-{version}-{arch}{ext} Amethyst/dist/amethyst=/usr/bin/amethyst Amethyst/dist/std=/usr/share/amethyst', False)
