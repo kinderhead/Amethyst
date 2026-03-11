f2535ef9303d8ea6190bbc2908fab719c7188c87
kinderhead <67707412+kinderhead@users.noreply.github.com>
2026-03-11 14:08:32 -0600

    Bump version

--- CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-* The empty function notice no longer happens.
-* Fixed a GC edge case when too many objects were allocated.
-* Added `static` variables.
--- scripts/send_release.py
+++ scripts/send_release.py
@@ -12,10 +12,26 @@ def call(cmd: str):
         raise Exception(ret.stderr)
     return ret.stdout
 
-os.chdir("docs")
-call("npm run update")
-os.chdir("..")
+# os.chdir("docs")
+# call("npm run update")
+# os.chdir("..")
+# call("git add .")
+# call("git commit -m \"Publish documentation for release\"")
+# call("git push")
+# call("gh workflow run Release")
+
+with open("CHANGELOG.md", "w+") as f:
+    f.write("")
+
+csproj = ET.parse("Amethyst/Amethyst.csproj")
+data = csproj.getroot()
+version = data.find("PropertyGroup").find("Version")  # type: ignore
+semver = version.text.split(".") # type: ignore
+
+semver[-1] = "-".join([str(int(semver[-1].split("-")[0]) + 1), *semver[-1].split("-")[1:]])
+version.text = ".".join(semver) # type: ignore
+csproj.write("Amethyst/Amethyst.csproj")
+
 call("git add .")
-call("git commit -m \"Publish documentation for release\"")
+call("git commit -m \"Bump version\"")
 call("git push")
-call("gh workflow run Release")
