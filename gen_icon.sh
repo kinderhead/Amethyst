579dcbbe73c3e9aafc42b170852a8d6ffafb4a96
kinderhead <67707412+kinderhead@users.noreply.github.com>
2026-04-29 20:19:09 -0600

    Added icons to the vscode extension

--- gen_icon.sh
+++ gen_icon.sh
@@ -7,6 +7,7 @@ inkscape=org.inkscape.Inkscape
 $inkscape -w 16 -h 16 -o tmp/16.png logo.svg
 $inkscape -w 32 -h 32 -o tmp/32.png logo.svg
 $inkscape -w 48 -h 48 -o tmp/48.png logo.svg
+$inkscape -w 256 -h 256 -o AmethystLanguageServer/logo.png logo.svg
 
 convert tmp/16.png tmp/32.png tmp/48.png icon.ico
 cp icon.ico docs/static/img/favicon.ico
