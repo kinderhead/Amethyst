5d922deae3208a4e8aa2e53fac8291031c233ab9
kinderhead <67707412+kinderhead@users.noreply.github.com>
2026-08-04 20:11:10 -0600

    Prep for a large refactor

--- Amethyst/AST/Expressions/CallExpression.cs
+++ Amethyst/AST/Expressions/CallExpression.cs
@@ -11,75 +11,56 @@ using Geode.Values;
 
 namespace Amethyst.AST.Expressions
 {
-	public class CallExpression(LocationRange loc, Expression func, List<Expression> args) : Expression(loc)
-	{
-		public readonly List<Expression> Args = args;
-		public readonly Expression Function = func;
+    public class CallExpression(LocationRange loc, Expression func, List<Expression> args) : Expression(loc)
+    {
+        public readonly List<Expression> Args = args;
+        public readonly Expression Function = func;
 
-		protected override ValueRef ExecuteImpl(FunctionContext ctx, TypeSpecifier? expected)
-		{
-			var func = ReferenceType.TryDeref(Function.Execute(ctx, null), ctx);
+        protected override ValueRef ExecuteImpl(FunctionContext ctx, TypeSpecifier? expected)
+        {
+            var func = ReferenceType.TryDeref(Function.Execute(ctx, null), ctx);
 
-			Expression[] newArgs;
+            Expression[] newArgs;
 
-			if (Function is PropertyExpression prop)
-			{
-				// Make sure the `this` parameter isn't dereferenced
-				newArgs =
-				[
-					new ValueRefExpression(prop.Expression.Location, prop.Expression.Execute(ctx, new VarType())),
-					.. Args
-				];
-			}
-			else
-			{
-				newArgs = [.. Args];
-			}
+            if (Function is PropertyExpression prop)
+            {
+                // Make sure the `this` parameter isn't dereferenced
+                newArgs =
+                [
+                    new ValueRefExpression(prop.Expression.Location, prop.Expression.Execute(ctx, new VarType())),
+                    .. Args
+                ];
+            }
+            else newArgs = [.. Args];
 
-			if (func.Value is Intrinsic i)
-			{
-				return i.Execute(ctx, [.. newArgs.Select(i => i.Execute(ctx, null))]);
-			}
+            if (func.Value is Intrinsic i) return i.Execute(ctx, [.. newArgs.Select(i => i.Execute(ctx, null))]);
 
-			ValueRef[]? args = null;
+            ValueRef[]? args = null;
 
-			if (func.Value is OverloadedFunctionValue overload)
-			{
-				args = [.. newArgs.Select(i => i.Execute(ctx, null))];
-				var types = TypeArray.From(args);
-				var options = overload.Get(types);
+            if (func.Value is OverloadedFunctionValue overload)
+            {
+                args = [.. newArgs.Select(i => i.Execute(ctx, null))];
+                var types = TypeArray.From(args);
+                var options = overload.Get(types);
 
-				if (options.Length == 0)
-				{
-					throw new NoOverloadError(overload.ID, types);
-				}
+                func = options.Length switch
+                {
+                    0 => throw new NoOverloadError(overload.ID, types),
+                    > 1 => throw new AmbiguousOverloadError(overload.ID, types),
+                    _ => Function is PropertyExpression
+                        ? ReferenceType.TryDeref(ctx.GetProperty(args[0], options[0].id.GetFile()), ctx)
+                        : new(ctx.GetVariable(options[0].id.ToString()))
+                };
+            }
 
-				if (options.Length > 1)
-				{
-					throw new AmbiguousOverloadError(overload.ID, types);
-				}
+            if (func.Type is not FunctionType type) throw new InvalidTypeError(func.Type.ToString(), "function");
 
-				// args[0] here is the `this` parameter
-				func = Function is PropertyExpression
-					? ReferenceType.TryDeref(ctx.GetProperty(args[0], options[0].id
-						.GetFile()), ctx)
-					: new(ctx.GetVariable(options[0].id.ToString()));
-			}
+            args ??= [.. newArgs.Zip(type.Parameters).Select(i => i.First.Execute(ctx, i.Second.Type))];
 
-			if (func.Type is not FunctionType type)
-			{
-				throw new InvalidTypeError(func.Type.ToString(), "function");
-			}
+            if (func.Value is FunctionValue f) return f.CallBehavior(ctx, args);
 
-			args ??= [.. newArgs.Zip(type.Parameters).Select(i => i.First.Execute(ctx, i.Second.Type))];
-
-			if (func.Value is FunctionValue f)
-			{
-				return f.CallBehavior(ctx, args);
-			}
-
-			ctx.Add(new PushFuncArgsInsn(type, ctx.PrepArgs(type, args)));
-			return ctx.Add(new DynCallInsn(func));
-		}
-	}
+            ctx.Add(new PushFuncArgsInsn(type, ctx.PrepArgs(type, args)));
+            return ctx.Add(new DynCallInsn(func));
+        }
+    }
 }
--- Geode/IR/Passes/InlinePass.cs
+++ Geode/IR/Passes/InlinePass.cs
@@ -4,43 +4,37 @@ using Geode.Values;
 
 namespace Geode.IR.Passes
 {
-	public class InlinePass : Pass
-	{
-		protected override void OnInsn(FunctionContext ctx, Block block, Instruction insn)
-		{
-			if (insn is CallInsn call && call.Arg<ValueRef>(0).Value is FunctionValue func &&
-			    func.FuncType.Modifiers.HasFlag(FunctionModifiers.Inline))
-			{
-				var other = ctx.Compiler.IR.Functions.Find(i => i.Decl.ID == func.ID);
+    public class InlinePass : Pass
+    {
+        protected override void OnInsn(FunctionContext ctx, Block block, Instruction insn)
+        {
+            if (insn is CallInsn call && call.Arg<ValueRef>(0).Value is FunctionValue func && func.FuncType.Modifiers.HasFlag(FunctionModifiers.Inline))
+            {
+                var other = ctx.Compiler.IR.Functions.Find(i => i.Decl.ID == func.ID);
 
-				if (other is null || other.Blocks.Count != 1 || other.Decl.FuncType.Parameters.Length > 0 ||
-				    other.Decl.FuncType.ReturnType is not VoidType)
-				{
-					return;
-				}
+                if (other is null || other.Blocks.Count != 1 || other.Decl.FuncType.Parameters.Length > 0 ||
+                    other.Decl.FuncType.ReturnType is not VoidType)
+                    return;
 
-				var (newInsns, newVariables) = other.Blocks.First().Copy("inline_frame");
+                var (newInsns, newVariables) = other.Blocks.First().Copy("inline_frame");
 
-				foreach (var i in newVariables)
-				{
-					ctx.RegisterExtraLocal(i);
-				}
+                foreach (var i in newVariables)
+                {
+                    ctx.RegisterExtraLocal(i);
+                }
 
-				for (var i = 0; i < newInsns.Count; i++)
-				{
-					if (newInsns[i] is ReturnInsn ret)
-					{
-						if (ret.ReturnType != new VoidType())
-						{
-							throw new NotImplementedException();
-						}
+                for (var i = 0; i < newInsns.Count; i++)
+                {
+                    if (newInsns[i] is ReturnInsn ret)
+                    {
+                        if (ret.ReturnType != new VoidType()) throw new NotImplementedException();
 
-						newInsns = newInsns[..i];
-					}
-				}
+                        newInsns = newInsns[..i];
+                    }
+                }
 
-				insn.ReplaceWith([.. newInsns]);
-			}
-		}
-	}
+                insn.ReplaceWith([.. newInsns]);
+            }
+        }
+    }
 }
